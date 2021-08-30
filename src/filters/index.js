cb808d0eff655c2753147dfee3836bfd8da63ff4
Mahsa Allahyari <allahyari@sabacell.ir>
2021-08-30 22:15:18 +0430

    Fix: change year to full years

--- src/filters/index.js
+++ src/filters/index.js
@@ -7,7 +7,7 @@ export default () => {
       const newDate = new Date(date)
       const monthNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
         'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      result = newDate.getDay() + 1 + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getYear()
+      result = newDate.getDay() + 1 + ' ' + monthNames[newDate.getMonth()] + ' ' + newDate.getFullYear()
     }
     return result
   })
